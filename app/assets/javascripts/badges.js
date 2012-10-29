d1e579dec1b01b68d505cf94f4533a444077178c
cone <coneramu@gmail.com>
2012-10-29 00:38:57 -0600

    add some ajax

--- app/assets/javascripts/badges.js
+++ app/assets/javascripts/badges.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-  /*$('#badges_info').click(function(){
+  $('#badges_info').click(function(){
     if($('#mockup_banner').hasClass('profile_banner')){
       $('#mockup_banner').removeClass('profile_banner').addClass('badges_banner');
       $('#mockup_banner #main_caption h3').text('Badges');
+      $.getJSON(
+        'badges/show',
+        function(info){
+          var content="";
+          for(var key in info){
+            for(var badge in info[key]){
+             content+='<div class="'+info[key][badge]+'">'+info[key][badge]+'</div>';
+            }
+          }
+          $('#badges_container').html(content);
+        });
     }
   });
   $('#profile_info').click(function(){
@@ -11,5 +22,5 @@ $(document).ready(function(){
       $('#mockup_banner #main_caption h3').text('Carlos Gutierrez');
     }
     
-  });*/
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 MinitestBadges::Application.routes.draw do
   root :to => 'badges#index'
+  resources 'badges', only: [:index, :show] , format: false
 end
 #== Route Map
-# Generated on 27 Oct 2012 20:09
+# Generated on 28 Oct 2012 22:57
 #
+# badges GET /badges     badges#index
+#  badge GET /badges/:id badges#show
