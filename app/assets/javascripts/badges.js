28adf664a67b86a95d3b4fc4de76b5dcb3d2e0dd
cone <coneramu@gmail.com>
2012-10-28 22:45:32 -0600

    Implement badges index and show actions

--- app/assets/javascripts/badges.js
+++ app/assets/javascripts/badges.js
@@ -1,6 +1,15 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-/*$('#container').isotope({
-  itemSelector: '.element',
-  layoutMode: 'fitRows'
-})*/
+$(document).ready(function(){
+  /*$('#badges_info').click(function(){
+    if($('#mockup_banner').hasClass('profile_banner')){
+      $('#mockup_banner').removeClass('profile_banner').addClass('badges_banner');
+      $('#mockup_banner #main_caption h3').text('Badges');
+    }
+  });
+  $('#profile_info').click(function(){
+    if($('#mockup_banner').hasClass('badges_banner')){
+      $('#mockup_banner').removeClass('badges_banner').addClass('profile_banner');
+      $('#mockup_banner #main_caption h3').text('Carlos Gutierrez');
+    }
+    
+  });*/
+});
--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,7 +1,12 @@
 class BadgesController < ApplicationController
+  respond_to :json
   
   def index
-
+    
   end
 
+  def show
+    data = {badges: ['workaholic','workaholic','workaholic']}
+    render json: data
+  end
 end
